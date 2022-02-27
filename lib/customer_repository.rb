9ff042c73c8f26afaf6d1cf4a6c838ec4d24c76b
James Harkins <jh.harkins@protonmail.com>
2022-02-27 15:04:53 -0700

    feat: create customer_repo find_all_by_last_name method that passes tests

--- lib/customer_repository.rb
+++ lib/customer_repository.rb
@@ -30,4 +30,8 @@ class CustomerRepository
     @customers.find_all {|customer| customer.first_name == first_name}
   end
 
+  def find_all_by_last_name(last_name)
+    @customers.find_all {|customer| customer.last_name == last_name}
+  end
+
 end
--- spec/customer_repository_spec.rb
+++ spec/customer_repository_spec.rb
@@ -49,8 +49,8 @@ RSpec.describe CustomerRepository do
 
   describe '#find_all_by_last_name' do
     it 'can find all customers with the same last name' do
-      expect(@cr.find_all_by_first_name("Miller").count).to eq(2)
-      expect(@cr.find_all_by_first_name("Miller")[0]).to be_a(Customer)
+      expect(@cr.find_all_by_last_name("Miller").count).to eq(2)
+      expect(@cr.find_all_by_last_name("Miller")[0]).to be_a(Customer)
     end
   end
 
