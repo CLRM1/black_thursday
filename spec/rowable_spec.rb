e4d190a089a55d5bd8457122d7dcc9e3a91d3ce2
Chris Romero <chrisleeromero1@gmail.com>
2022-03-02 18:34:44 -0700

    Test: update rowable test to look for file attributes

--- spec/rowable_spec.rb
+++ spec/rowable_spec.rb
@@ -1,12 +1,13 @@
-# require_relative '../lib/rowable'
-# require 'simplecov'
-# SimpleCov.start
-#
-# RSpec.describe Rowable do
-#   describe "Iteration 1" do
-#     xit "exits" do
-#       repository_module = Rowable.new
-#       expect(repository_module).to be_a(Rowable)
-#     end
-#   end
-# end
+require_relative '../lib/rowable'
+require_relative '../lib/merchant_repository'
+require 'simplecov'
+SimpleCov.start
+
+RSpec.describe Rowable do
+  describe "Iteration 1" do
+    it "can read atttributes" do
+      @merchant_repo = MerchantRepository.new('./data/mini_merchants.csv')
+      expect(@merchant_repo.filename).to eq('./data/mini_merchants.csv')
+    end
+  end
+end
